0d0759938dcdfb14fe5844a8b6300d3f2428b213
rg261992 <rohanguptaishere@gmail.com>
2018-06-05 20:29:36 -0400

    Implementing logic of caching manager

---
+++ CachingManager.m
@@ -0,0 +1,59 @@
+//
+//  CachingManager.m
+//
+//
+//
+
+#import "CachingManager.h"
+#import "CachingException.h"
+#import "NSObject+CacheBuilder.h"
+
+@interface CachingManager()
+{
+    dispatch_queue_t serialQueue;
+}
+
+@end
+
+@implementation CachingManager
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        serialQueue = dispatch_queue_create("com.flipkart.caching", DISPATCH_QUEUE_SERIAL);
+    }
+    return self;
+}
+
+- (BOOL) cacheValue:(Value*) value forKey:(NSString *)key;
+{
+    __block BOOL cached = NO;
+    dispatch_sync(serialQueue, ^{
+        cached = [self.firstResponderCacheAlgo cacheValue:value forKey:key];
+    });
+    
+    return cached;
+}
+
+- (Value*) getValueForKey:(NSString *) key;
+{
+    __block Value *value = nil;
+    dispatch_sync(serialQueue, ^{
+        value = [self.firstResponderCacheAlgo getValueForKey:key];
+    });
+    
+    return value;
+}
+
+- (void) clearCacheAndCascade;
+{
+    dispatch_sync(serialQueue, ^{
+        [self.firstResponderCacheAlgo clearCacheAndCascade];
+    });
+}
+
+
+@end
+
