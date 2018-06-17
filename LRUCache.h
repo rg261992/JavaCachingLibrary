110c735b49287cb7eea7ecdf693de226ae80fb52
rg261992 <rohanguptaishere@gmail.com>
2018-06-17 00:13:59 -0400

    implementation of lru cache

---
+++ LRUCache.h
@@ -0,0 +1,30 @@
+//
+//  LRUCache.h
+//  Pods
+//
+//  Created by Prabodh Prakash on 01/09/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "Node.h"
+#import "CachingDatabaseHandler.h"
+#import "CachingDiskHandler.h"
+#import "BaseCache.h"
+
+/*!
+ This class provides standard implementation of LRU Cache.
+ */
+@interface LRUCache : BaseCache
+
+/*!
+ The maximum number of elements that can be stored in memory.
+ */
+@property (nonatomic, assign) NSInteger maxElementsInMemory;
+
+/*!
+ The maximum memory that can be used to store elements
+ */
+@property (nonatomic, assign) float maxMemoryAllocated;
+
+@end
